56f278f0e9665c0e43faa68c2b70dd2fce741aeb
victorap-tech <victorapostoles@gmail.com>
2026-02-10 18:19:53 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
