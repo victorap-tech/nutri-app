e7ae0883f968c39d862bf1bc78faec66bba20492
victorap-tech <victorapostoles@gmail.com>
2026-02-10 10:57:59 -0300

    Create plan_alimento.py

---
+++ backend/plan_alimento.py
@@ -0,0 +1,9 @@
+from database import db
+
+class PlanAlimento(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    plan_id = db.Column(db.Integer, db.ForeignKey("plan_alimentario.id"), nullable=False)
+    alimento_id = db.Column(db.Integer, db.ForeignKey("alimento.id"), nullable=False)
+    comida = db.Column(db.String(50), nullable=False)  # desayuno, almuerzo, etc.
+
+    alimento = db.relationship("Alimento")
