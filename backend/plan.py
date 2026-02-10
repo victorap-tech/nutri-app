c315474972868743534bdf930b8d128461647e30
victorap-tech <victorapostoles@gmail.com>
2026-02-10 10:56:59 -0300

    Create plan.py

---
+++ backend/plan.py
@@ -0,0 +1,8 @@
+from database import db
+
+class PlanAlimentario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    paciente_id = db.Column(db.Integer, db.ForeignKey("paciente.id"), nullable=False)
+    fecha = db.Column(db.Date, nullable=False)
+
+    paciente = db.relationship("Paciente", backref="planes")
