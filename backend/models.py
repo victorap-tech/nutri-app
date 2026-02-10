6fa64ba65bde6a901b26fad9e6ba4cfe26dfe3a8
victorap-tech <victorapostoles@gmail.com>
2026-02-10 16:36:57 -0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,14 @@
 from database import db
 
+from database import db
+
 class Paciente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100))
-    apellido = db.Column(db.String(100))
+
+    nombre = db.Column(db.String(100), nullable=False)
+    apellido = db.Column(db.String(100), nullable=False)
+    dni = db.Column(db.String(20), unique=True, nullable=False)
+
     edad = db.Column(db.Integer)
     altura = db.Column(db.Float)
     peso = db.Column(db.Float)
