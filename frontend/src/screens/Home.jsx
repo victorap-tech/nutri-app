654139c5064c032ec2d6b378a892acaf50316034
victorap-tech <victorapostoles@gmail.com>
2026-02-10 15:53:19 -0300

    Create Home.jsx

---
+++ frontend/src/screens/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Nutri App</h1>
+
+      <p>
+        <strong>Estado backend:</strong> Nutri App OK
+      </p>
+
+      <button style={{ marginTop: "1rem" }}>
+        Pacientes
+      </button>
+    </div>
+  );
+}
+
+export default Home;
