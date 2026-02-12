b28acd60f3c30cd38e67d673bd7dbd720009e2b9
victorap-tech <victorapostoles@gmail.com>
2026-02-12 10:40:41 -0300

    Add Home route and new patient route to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 import PacienteDetalle from "./pages/PacienteDetalle";
 import PacienteEvolucion from "./pages/PacienteEvolucion";
 
@@ -6,8 +8,19 @@ function App() {
   return (
     <Router>
       <Routes>
+
+        {/* HOME */}
+        <Route path="/" element={<Home />} />
+
+        {/* NUEVO PACIENTE */}
+        <Route path="/paciente/nuevo" element={<PacienteDetalle nuevo />} />
+
+        {/* FICHA PACIENTE */}
         <Route path="/paciente/:id" element={<PacienteDetalle />} />
+
+        {/* EVOLUCIÓN */}
         <Route path="/paciente/:id/evolucion" element={<PacienteEvolucion />} />
+
       </Routes>
     </Router>
   );
