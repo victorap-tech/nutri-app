e347e9a0b5953b57a00f50d0de35c7a901be93e0
victorap-tech <victorapostoles@gmail.com>
2026-02-12 11:52:28 -0300

    Add NuevoPaciente route and remove PacienteEvolucion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PacienteDetalle from "./pages/PacienteDetalle";
-import PacienteEvolucion from "./pages/PacienteEvolucion";
+import NuevoPaciente from "./pages/NuevoPaciente";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Home */}
         <Route path="/" element={<Home />} />
+
+        {/* Crear paciente */}
+        <Route path="/nuevo" element={<NuevoPaciente />} />
+
+        {/* Detalle paciente */}
         <Route path="/paciente/:id" element={<PacienteDetalle />} />
-        <Route path="/paciente/:id/evolucion" element={<PacienteEvolucion />} />
+
+        {/* Ruta fallback */}
+        <Route
+          path="*"
+          element={
+            <div style={{ padding: "40px", textAlign: "center" }}>
+              <h2>Página no encontrada</h2>
+              <a href="/" style={{ color: "#2c7be5" }}>
+                Volver al inicio
+              </a>
+            </div>
+          }
+        />
       </Routes>
     </Router>
   );
