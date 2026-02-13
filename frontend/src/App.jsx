63bf6ab96fd1304dcb1b3f8eba90a49fbadd2fc2
victorap-tech <victorapostoles@gmail.com>
2026-02-12 22:42:04 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import NuevoPaciente from "./pages/NuevoPaciente";
 import PacienteDetalle from "./pages/PacienteDetalle";
-import EvolucionPaciente from "./pages/EvolucionPaciente";
+import PacienteEvolucion from "./pages/PacienteEvolucion";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Home */}
         <Route path="/" element={<Home />} />
+
+        {/* Nuevo paciente */}
         <Route path="/nuevo" element={<NuevoPaciente />} />
+
+        {/* Detalle paciente */}
         <Route path="/paciente/:id" element={<PacienteDetalle />} />
-        <Route path="/paciente/:id/evolucion" element={<EvolucionPaciente />} />
+
+        {/* Evolución paciente */}
+        <Route
+          path="/paciente/:id/evolucion"
+          element={<PacienteEvolucion />}
+        />
       </Routes>
     </Router>
   );
