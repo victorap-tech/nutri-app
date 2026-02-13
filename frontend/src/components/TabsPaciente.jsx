833938e5ce4094e8b9bbd342aef0e5f9b107dd9b
victorap-tech <victorapostoles@gmail.com>
2026-02-13 00:11:09 -0300

    Update TabsPaciente.jsx

--- frontend/src/components/TabsPaciente.jsx
+++ frontend/src/components/TabsPaciente.jsx
@@ -1,1 +1,14 @@
+import { NavLink, useParams } from "react-router-dom";
 
+export default function TabsPaciente() {
+  const { id } = useParams();
+
+  return (
+    <div className="tabs">
+      <NavLink to={`/pacientes/${id}`} end>Ficha</NavLink>
+      <NavLink to={`/pacientes/${id}/evolucion`}>Evolución</NavLink>
+      <NavLink to={`/pacientes/${id}/plan`}>Plan</NavLink>
+      <NavLink to={`/pacientes/${id}/laboratorio`}>Laboratorio</NavLink>
+    </div>
+  );
+}
