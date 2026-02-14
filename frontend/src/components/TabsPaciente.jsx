87c3a135aaccd1cb2f86a84d612a0d3047294aba
victorap-tech <victorapostoles@gmail.com>
2026-02-14 13:18:19 -0300

    Update TabsPaciente.jsx

--- frontend/src/components/TabsPaciente.jsx
+++ frontend/src/components/TabsPaciente.jsx
@@ -3,12 +3,15 @@ import { NavLink, useParams } from "react-router-dom";
 export default function TabsPaciente() {
   const { id } = useParams();
 
+  const tabClass = ({ isActive }) =>
+    "tab-btn " + (isActive ? "active" : "");
+
   return (
     <div className="tabs">
-      <NavLink to={`/pacientes/${id}`} end>Ficha</NavLink>
-      <NavLink to={`/pacientes/${id}/evolucion`}>Evolución</NavLink>
-      <NavLink to={`/pacientes/${id}/plan`}>Plan</NavLink>
-      <NavLink to={`/pacientes/${id}/laboratorio`}>Laboratorio</NavLink>
+      <NavLink className={tabClass} to={`/pacientes/${id}`}>Ficha</NavLink>
+      <NavLink className={tabClass} to={`/pacientes/${id}/evolucion`}>Evolución</NavLink>
+      <NavLink className={tabClass} to={`/pacientes/${id}/plan`}>Plan</NavLink>
+      <NavLink className={tabClass} to={`/pacientes/${id}/laboratorio`}>Laboratorio</NavLink>
     </div>
   );
 }
