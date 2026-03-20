6ab69d8c8bc4806b29689876f86bfae87272286d
victorap-tech <victorapostoles@gmail.com>
2026-03-20 15:17:13 -0300

    Update plan_alimento.py

--- backend/plan_alimento.py
+++ backend/plan_alimento.py
@@ -1,21 +1,11 @@
 from database import db
 
 class PlanAlimento(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    plan_id = db.Column(
-        db.Integer,
-        db.ForeignKey("plan_alimentario.id"),
-        nullable=False
-    )
-
-    alimento_id = db.Column(
-        db.Integer,
-        db.ForeignKey("alimento.id"),
-        nullable=False
-    )
-
-    comida = db.Column(db.String(50), nullable=False)
-    cantidad = db.Column(db.String(100))  # 🔥 IMPORTANTE
-
-    alimento = db.relationship("Alimento")
+    __tablename__ = "plan_alimento"
+    id         = db.Column(db.Integer, primary_key=True)
+    plan_id    = db.Column(db.Integer, db.ForeignKey("plan_alimentario.id"), nullable=False)
+    alimento_id = db.Column(db.Integer, db.ForeignKey("alimento.id"), nullable=False)
+    comida     = db.Column(db.String(50))
+    cantidad   = db.Column(db.String(100))
+    dia        = db.Column(db.String(20), default="Todos")
+    alimento   = db.relationship("Alimento", lazy="select")
